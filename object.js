d6a9e338b82dbd4e6bda8671c878131cb79b32de
Suryajs102 <142585509+Suryajs102@users.noreply.github.com>
2023-08-22 13:18:17 +0530

    upload
    test

---
+++ object.js
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+<h2>Access a JavaScript object</h2>
+<p id="demo"></p>
+
+<script>
+const myObj = name:"John", age:30, city:"New York";
+document.getElementById("demo").innerHTML = myObj.name;
+</script>
+
+</body>
+</html>
